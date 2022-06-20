eefce701fee612bc963b29b28f79758bc661faec
AmandaAzania <mandygudlwa@gmail.com>
2022-06-20 14:30:05 +0200

    fix

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 let numbers = [1, 2, 3,4,5,6,7,8,8,9];
   let iterator = numbers.values();
-  let one = iterator.next().value;
-  let two = iterator.next().value;
-  let three = iterator.next().value;
-console.log(one);
-console.log(two);
-console.log(three);
+  let first = iterator.next().value;
+  let second = iterator.next().value;
+  let third = iterator.next().value;
+console.log(first);
+console.log(second);
+console.log(third);
